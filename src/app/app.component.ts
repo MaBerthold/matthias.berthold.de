f3225028b8cc807c889f33f741a8863e3e1e3145
Matthias Berthold <matthiasberthold33@gmail.com>
2023-11-24 13:52:38 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'matthiasberthold.de';
+
+  images = ['house.jpg', 'road.jpg', 'speicherstadt.jpg'];
+ 
+  ngOnInit(): void {
+    
+  }
+
+
+
 }
